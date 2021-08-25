e9c6022b29c2298aa957a901497fe2fc7e9308ee
Tomasz Bednorz <tomekbed99@gmail.com>
2021-08-25 11:20:55 +0200

    added init function to ssd1306

--- Core/Inc/ssd1306.h
+++ Core/Inc/ssd1306.h
@@ -21,36 +21,77 @@ extern I2C_HandleTypeDef hi2c1;
 #define SSD1306_CTRL_BYTE_COMM		0x00
 #define SSD1306_CTRL_BYTE_DATA		0x40
 #define SSD1306_MEM_ADDR_SIZE		1
+#define SSD1306_COMM_DATA_SIZE		1
+
+/*
+ *  Helpful defines connected with display
+ */
+
+#define SSD1306_COLUMNS				128
+#define SSD1306_ROWS				64
+#define SSD1306_PAGES				8
 
 /*
  *  Commands from SSD1306 driver datasheet
  */
 
-// Fundamental command
-#define SSD1306_SET_CONTRAST_COLOR		0x81
+// Fundamental commands
+#define SSD1306_SET_CONTRAST_COLOR			0x81
 
-#define SSD1306_RAM_CONTENT_DISPLAY		0xA4
-#define SSD1306_ENTIRE_DISPLAY_ON		0x05
+#define SSD1306_RAM_CONTENT_DISPLAY			0xA4
+#define SSD1306_ENTIRE_DISPLAY_ON			0x05
 
-#define SSD1306_SET_NORMAL_DISPLAY		0xA6
-#define SSD1306_SET_INVERSE_DISPLAY		0xA7
+#define SSD1306_SET_NORMAL_DISPLAY			0xA6
+#define SSD1306_SET_INVERSE_DISPLAY			0xA7
 
-#define SSD1306_SET_DISPLAY_OFF			0xAE
-#define SSD1306_SET_DISPLAY_ON			0xAF
+#define SSD1306_SET_DISPLAY_OFF				0xAE
+#define SSD1306_SET_DISPLAY_ON				0xAF
 
-// Scrolling command
+// Scrolling commands
+#define SSD1306_RIGHT_HORIZONTAL_SCROLL		0x26
+#define SSD1306_LEFT_HOTIZONTAL_SCROLL		0x27
 
+#define SSD1306_VERTICAL_AND_RIGHT_HORIZONTAL_SCROLL	0x29
+#define SSD1306_VERTICAL_AND_LEFT_HORIZONTAL_SCROLL		0x2A
 
+#define SSD1306_DEACTIVE_SCROLL				0x2E
+#define SSD1306_ACTIVE_SCROLL				0x2F
 
+#define SSD1306_SET_VERTICAL_SCROLL_AREA	0xA3
 
+// Addressing setting commands
+#define SSD1306_SET_LOWER_COLUMN			0x00
+#define SSD1306_SET_HIGHER_COLUMN			0x10
+#define SSD1306_SET_MEMORY_MODE				0x20
+#define SSD1306_SET_COLUMN_ADDRESS			0x21
+#define SSD1306_SET_PAGE_ADDRESS			0x22
 
+// Hardware configuration commands
+#define SSD1306_SET_DISPLAY_START_LINE		0x40
+#define SSD1306_COL0_MAPPED_SEG0			0xA0
+#define SSD1306_COL0_MAPPED_SEG127			0xA1
+#define SSD1306_SET_MULTIPLEX_RATIO			0xA8
+#define SSD1306_NORMAL_MODE					0xC0
+#define SSD1306_REMAPPED_MODE				0xC3
+#define SSD1306_SET_DISPLAY_OFFSET			0xD3
+#define SSD1306_SET_COM_PINS				0xDA
+
+// Timing & Driving scheme setting commands
+#define SSD1306_SET_DISPLAY_CLOCK_DIV		0xD5
+#define SSD1306_SET_PRE_CHARGE_PERIOD		0xD9
+#define SSD1306_SET_V_COMH_DESELECT_LEVEL	0xDB
+#define SSD1306_NO_OPERATION				0xE3
+
+// Charge Bump Setting commands
+#define SSD1306_CHARGE_PUMP_SETTING			0x8D
+#define SSD1306_ENABLE_CHARGE_PUMP			0x14
 
 /*
  *  Function prototypes
  */
-bool ssd1306_init(void);
-
-
+void ssd1306_init(void);
+void ssd1306_display_clear(void);
+void SSD1306_init2(void);
 
 
 #endif /* INC_SSD1306_H_ */
--- Core/Src/ssd1306.c
+++ Core/Src/ssd1306.c
@@ -1,17 +1,15 @@
 #include "ssd1306.h"
 
 
-
-
-
-
+// Buffer contains display content
+uint8_t buffer[SSD1306_COLUMNS * SSD1306_PAGES];
 
 /*
  *  Static functions prototypes
  */
 
-void ssd1306_write_command(uint8_t *command);
-void ssd1306_write_data(uint8_t *data);
+void ssd1306_write_command(uint8_t command);
+void ssd1306_write_data(uint8_t *data, uint16_t length);
 
 /*
  *  Function definitions
@@ -20,17 +18,65 @@ void ssd1306_write_data(uint8_t *data);
 /*
  * @fn      		  - ssd1306_init
  *
- * @return            - true: correct initialization / false: incorrect initialization
+ * @return            - None
  *
  * @Note              - This is SSD1306 initialization function
  */
-bool ssd1306_init(void)
+void ssd1306_init(void)
 {
+	ssd1306_display_clear();
 
+	// Display Off
+	ssd1306_write_command(SSD1306_SET_DISPLAY_OFF);
+
+	// Set Display Clock Div
+	ssd1306_write_command(SSD1306_SET_DISPLAY_CLOCK_DIV);
+	ssd1306_write_command(0xE0);
+
+	// Set MUX Ratio
+	ssd1306_write_command(SSD1306_SET_MULTIPLEX_RATIO);
+	ssd1306_write_command(SSD1306_ROWS - 1);
+
+	// Set Display Offset
+	ssd1306_write_command(SSD1306_SET_DISPLAY_OFFSET);
+	ssd1306_write_command(0x00);
+
+	// Set Display Start Line
+	ssd1306_write_command(SSD1306_SET_DISPLAY_START_LINE);
+
+	// Set Segment re-map
+	ssd1306_write_command(SSD1306_COL0_MAPPED_SEG0);
+
+	// Set COM Output Scan Direction
+	ssd1306_write_command(SSD1306_NORMAL_MODE);
+
+	// Set COM Pins hardware configuration
+	ssd1306_write_command(SSD1306_SET_COM_PINS);
+	ssd1306_write_command(0x02);
+
+	// Set Contrast Color
+	ssd1306_write_command(SSD1306_SET_CONTRAST_COLOR);
+	ssd1306_write_command(0x7F);
+
+	// Disable Entire Display On
+	ssd1306_write_command(SSD1306_RAM_CONTENT_DISPLAY);
+
+	// Set Normal Display
+	ssd1306_write_command(SSD1306_SET_NORMAL_DISPLAY);
+
+	// Enable charge pump regulator
+	ssd1306_write_command(SSD1306_CHARGE_PUMP_SETTING);
+	ssd1306_write_command(SSD1306_ENABLE_CHARGE_PUMP);
+
+	// Display On
+	ssd1306_write_command(SSD1306_SET_DISPLAY_ON);
 }
 
 
-
+void ssd1306_display_clear(void)
+{
+	memset(buffer, 0, sizeof(buffer));
+}
 
 
 
@@ -39,19 +85,19 @@ bool ssd1306_init(void)
  *  Static functions
  */
 
-void ssd1306_write_command(uint8_t *command)
+void ssd1306_write_command(uint8_t command)
 {
 	if(HAL_I2C_Mem_Write(&SSD1306_I2C_HANDLE, SSD1306_I2C_ADDRESS, SSD1306_CTRL_BYTE_COMM,
-						 SSD1306_MEM_ADDR_SIZE, command, strlen((const char *)command), SSD1306_I2C_MAX_DELAY) != HAL_OK)
+			SSD1306_MEM_ADDR_SIZE, &command, SSD1306_COMM_DATA_SIZE, SSD1306_I2C_MAX_DELAY) != HAL_OK)
 	{
 		error_handler();
 	}
 }
 
-void ssd1306_write_data(uint8_t *data)
+void ssd1306_write_data(uint8_t *data, uint16_t length)
 {
 	if(HAL_I2C_Mem_Write(&SSD1306_I2C_HANDLE, SSD1306_I2C_ADDRESS, SSD1306_CTRL_BYTE_DATA,
-						 SSD1306_MEM_ADDR_SIZE, data, strlen((const char *)data), SSD1306_I2C_MAX_DELAY) != HAL_OK)
+						 SSD1306_MEM_ADDR_SIZE, data, length, SSD1306_I2C_MAX_DELAY) != HAL_OK)
 	{
 		error_handler();
 	}
